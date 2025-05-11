61b62e809db5f2b5aecbe2386bbcda21db075a50
Ashwathaman <ashwathaman.r6@gmail.com>
2025-05-12 00:33:47 +0530

    connection URI chaged

--- db.js
+++ db.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 const Recipe = require('./Models/Recipe.model');
 
 // Replace with your actual MongoDB Atlas username and password
-const username = 'ashwathamanr6';
-const password = '0EqI3qLt4i8f1ix3';
+// const username = 'ashwathamanr6';
+// const password = '0EqI3qLt4i8f1ix3';
 
-const uri = `mongodb+srv://${username}:<${password}>@your-cluster.mongodb.net/sample_mflix?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://ashwathamanr6:0EqI3qLt4i8f1ix3@cluster0.82ryhpa.mongodb.net/`;
+
+
 
 mongoose.connect(uri, {
   useNewUrlParser: true,
