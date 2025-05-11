28f93e3125462b80f22cf410760747a56478f8ef
Ashwathaman <ashwathaman.r6@gmail.com>
2025-05-11 23:52:18 +0530

    push for prod db change

--- Models/Recipe.model.js
+++ Models/Recipe.model.js
@@ -1,11 +1,22 @@
-const m = require('mongoose');
+const mongoose = require('mongoose');
 
-const RecipeSchema = new m.Schema({
-    Recipe_key: {type: Number, required: true, unique: true}, 
-    RecipeName: {type: String, required: true},
-    Ingredients: {type: Array, required: true},
-    Recipetype: {type: String, enum: ['veg', 'non-veg']},
-},{ timestamps: true })
+// Replace with your actual MongoDB Atlas username and password
+const username = encodeURIComponent('ashwathamanr6');
+const password = encodeURIComponent('0EqI3qLt4i8f1ix3');
+
+// Your cluster URL and DB name
+const clusterUrl = 'atlas-sql-6820e32e832ab3049376222f-htlpyt.a.query.mongodb.net';
+const dbName = 'sample_mflix';
+
+const uri = `mongodb://${username}:${password}@${clusterUrl}/${dbName}?ssl=true&authSource=admin`;
+
+const RecipeSchema = mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('MongoDB connected');
+    })
+    .catch(err => {
+        console.error('Connection error:', err);
+    });
 
 const books = m.model('Recipes', RecipeSchema, 'Recipes')
 
